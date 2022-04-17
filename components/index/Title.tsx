ba6e270334921a4c051aff6d6c280b4ae55bba11
The-Briel-Deal <gabe@fordltc.net>
2022-04-17 18:58:14 -0400

    Publishing my implementation of typewriter however I am going to move to a proven library

--- components/index/Title.tsx
+++ components/index/Title.tsx
@@ -1,5 +1,25 @@
 import styles from '../../styles/Home.module.css'
+import { useEffect, useState } from 'react';
+const descriptions = [' building things to make lives better.', ' cryptography and security.', ' cloud technologies.']
+
 export default function Title() {
+  const [description, setDescription] = useState('')
+  // Can go through 3 types of status, typing, deleting, and waiting.
+  const currentDescription = 0
+  const currentDescriptionStatus = 'typing'
+  const type = () => {
+    setDescription((x)=>{
+      if (x.length+1 !== descriptions[currentDescription].length) {
+        setTimeout(type, 100)
+      } else {
+
+      }
+      return (x + descriptions[currentDescription].charAt(x.length + 1));
+    })
+  }
+  useEffect(()=>{
+    type()
+  }, [])
   return (
     <>
       <h1 className={styles.title}>
@@ -7,7 +27,7 @@ export default function Title() {
       </h1>
       <p className={styles.description}>
         <code className={styles.code}>
-          A Software Engineer that loves building things to make lives better.
+          A Software Engineer that loves {description}
         </code>
       </p>
     </>
