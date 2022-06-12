97c7a10915c5ba380b2fb08de4effa9813c8712e
The-Briel-Deal <gabe@fordltc.net>
2022-06-12 23:59:34 +0200

    commit

--- components/index/Title.tsx
+++ components/index/Title.tsx
@@ -1,15 +1,34 @@
-import styles from '../../styles/Home.module.css'
-const descriptions = [' building things to make lives better.', ' cryptography and security.', ' cloud technologies.']
+import { useEffect, useRef, useState } from "react";
+import styles from "../../styles/Home.module.css";
+import { annotate } from "rough-notation";
+const descriptions = [
+  " building things to make lives better.",
+  " cryptography and security.",
+  " cloud technologies.",
+];
 
 export default function Title() {
+  const myName = useRef(null);
+  useEffect(() => {
+    if (myName?.current) {
+      const annotateMyName = annotate(myName.current, { type: "underline" });
+      annotateMyName.show();
+    } else {
+      console.log("myName is not ready");
+    }
+  }, []);
   return (
     <>
       <h1 className={styles.title}>
-        My name is <span className={styles.name}>Gabriel</span>
+        My name is{" "}
+        <span ref={myName} className={styles.name}>
+          Gabriel
+        </span>
       </h1>
       <p className={styles.description}>
         <code className={styles.code}>
-          A Software Engineer that loves building things to make lives better.
+          I&apos;m A Software Engineer that loves building things to make lives
+          better.
         </code>
       </p>
     </>
--- pages/index.tsx
+++ pages/index.tsx
@@ -4,8 +4,15 @@ import styles from "../styles/Home.module.css";
 import Title from "../components/index/Title";
 import Footer from "../components/Footer";
 import { Parallax, ParallaxLayer } from "@react-spring/parallax";
-
+import { animated, useSpring } from "@react-spring/web";
 const Home: NextPage = () => {
+  const props = useSpring({
+    config: { mass: 5, tension: 2000, friction: 200 },
+    opacity: true ? 1 : 0,
+    x: true ? 0 : 20,
+    height: true ? 110 : 0,
+    from: { opacity: 0, x: 20, height: 0 },
+  });
   return (
     <div className={styles.container}>
       <Head>
@@ -17,7 +24,7 @@ const Home: NextPage = () => {
         <Parallax pages={3} style={{ top: "0", left: "0" }}>
           <ParallaxLayer
             offset={0}
-            speed={2.5}
+            speed={1}
             style={{
               display: "flex",
               flexDirection: "column",
@@ -28,11 +35,50 @@ const Home: NextPage = () => {
             <Title />
           </ParallaxLayer>
 
-          <ParallaxLayer
-            offset={1}
-            speed={2}
-            style={{ backgroundColor: "#ff6d6d" }}
-          />
+          <ParallaxLayer offset={0.7} speed={2}>
+            <div className="custom-shape-divider-bottom-1654974253">
+              <svg
+                data-name="Layer 1"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 1200 120"
+                preserveAspectRatio="none"
+              >
+                <path
+                  d="M985.66,92.83C906.67,72,823.78,31,743.84,14.19c-82.26-17.34-168.06-16.33-250.45.39-57.84,11.73-114,31.07-172,41.86A600.21,600.21,0,0,1,0,27.35V120H1200V95.8C1132.19,118.92,1055.71,111.31,985.66,92.83Z"
+                  className="shape-fill"
+                ></path>
+              </svg>
+            </div>
+            <div
+              style={{
+                backgroundColor: "black",
+                display: "flex",
+                justifyContent: "center",
+                height: "100%",
+                marginTop: -5,
+              }}
+            >
+              <animated.h1
+                className={styles.title}
+                style={{ color: "white", ...props }}
+              >
+                Here&apos;s some of my work!
+              </animated.h1>
+            </div>
+            <div className="custom-shape-divider-top-1654975218">
+              <svg
+                data-name="Layer 1"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 1200 120"
+                preserveAspectRatio="none"
+              >
+                <path
+                  d="M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V0H0V27.35A600.21,600.21,0,0,0,321.39,56.44Z"
+                  className="shape-fill"
+                ></path>
+              </svg>
+            </div>
+          </ParallaxLayer>
 
           <ParallaxLayer
             offset={2}
